f3a10442991823e931cc944742ed3adba0105b3a
maheshthippala <mahesh.cubex@gmail.com>
2018-07-20 09:03:42 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/fragment_july730am/HomeFrag.kt
@@ -0,0 +1,17 @@
+package cubex.mahesh.fragment_july730am
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class  HomeFrag:Fragment( )
+{
+    override fun onCreateView(inflater: LayoutInflater,
+          container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var v = inflater.inflate(R.layout.home_frag,container,
+                false)
+        return v
+    }
+}
